756f67ba4ff02b0c5c67a1f8a9b1b63aa4b64256
hamlet-lab <248194637+hamlet-lab@users.noreply.github.com>
2026-09-02 00:37:21 +0900

    공개 벤치마크 API 내보내기 확장

--- src/sea_ops_arena/__init__.py
+++ src/sea_ops_arena/__init__.py
@@ -1,12 +1,20 @@
 """SEA Ops Arena의 공개 인터페이스와 벤치마크 하네스."""
 
+from .benchmark import BenchmarkRecord, BenchmarkRun, run_suite
 from .contracts import DecisionReceipt, DecisionStatus, ExecutionRequest, ExecutionResult, ExecutionStatus
 from .interfaces import ControllerAdapter, EnvironmentAdapter
 from .runner import ArenaRunner, ArenaTurn
+from .scenarios import BenchmarkCase, BenchmarkSuite
+from .scoring import CaseScore, ScoreSummary, score_case, summarize
 
 __all__ = [
     "ArenaRunner",
     "ArenaTurn",
+    "BenchmarkCase",
+    "BenchmarkRecord",
+    "BenchmarkRun",
+    "BenchmarkSuite",
+    "CaseScore",
     "ControllerAdapter",
     "DecisionReceipt",
     "DecisionStatus",
@@ -14,4 +22,8 @@ __all__ = [
     "ExecutionRequest",
     "ExecutionResult",
     "ExecutionStatus",
+    "ScoreSummary",
+    "run_suite",
+    "score_case",
+    "summarize",
 ]
